ae80891ed1f2c939498377bfcfc26d5360cd357c
Jan Odvarko <odvarko@gmail.com>
2013-02-18 16:08:49 +0100

    Test Firefox extension for bug 529536

---
+++ mozilla/bug529536/defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("extensions.logging.enabled", true);
+pref("dom.report_all_js_exceptions", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+

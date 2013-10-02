95f9e0ec73e540f1820cc58340cbddaa61921506
Jan Odvarko <odvarko@gmail.com>
2013-10-02 14:59:21 +0200

    Test files for issue 6832

---
+++ issues/6832/test.js
@@ -0,0 +1,11 @@
+var boj = {};
+boj.m1 = (function() {
+    return {
+        xxx : function(){return {};}
+    };
+})();
+//function f(){
+var par = boj.m1.xxx();
+console.assert(false);
+console.assert(false);
+//}f();

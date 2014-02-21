f843d9afc5028f921e7e72f60251c68e0ceab2d4
Jan Odvarko <odvarko@gmail.com>
2014-02-22 00:30:28 +0100

    Test files for issue 1238

---
+++ issues/1238/issue1238-2.js
@@ -0,0 +1,1 @@
+function add2(a, b) { var test = 'hello'; console.log(test); var c = a + b; console.log(c); return c; }
---
+++ issues/1238/issue1238.js
@@ -0,0 +1,4 @@
+function add(a, b) {
+    var test = 'hello'; console.log(test); /* this is a comment */ var c = a + b;
+    /* another comment */ console.log(c); return c;
+}

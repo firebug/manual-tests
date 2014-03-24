e388ee710f89daabc27f7f4d0a600d5f071badc6
Sebastian Zartner <sebastianzartner@gmail.com>
2014-03-24 09:50:18 +0100

    Test case for issue 3791 (Breakpoints in Scripts that have been redirected do not work correctly) http://code.google.com/p/fbug/issues/detail?id=3791

---
+++ issues/3791/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Firebug Test - Redirecting Script</title>
+    <script src="testJsRedirect.php"></script>
+</head>
+<body>
+    <p>Redirecting script test page</p>
+</body>
+</html>
---
+++ issues/3791/js/test.js
@@ -0,0 +1,3 @@
+( function() {
+    console.log( 'hey! set a breakpoint here if you can!' );
+} )();
